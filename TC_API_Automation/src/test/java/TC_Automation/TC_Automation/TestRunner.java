4b5089bde1190beba586e4269064181e595c9d2d
4482E <4482E@CPLF4482E-PC.thyrocare.com>
2022-10-11 18:40:39 +0530

    Added New Feature

--- TC_API_Automation/src/test/java/TC_Automation/TC_Automation/TestRunner.java
+++ TC_API_Automation/src/test/java/TC_Automation/TC_Automation/TestRunner.java
@@ -1,44 +1,85 @@
 package TC_Automation.TC_Automation;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 
 
-public class TestRunner {
+public class TestRunner 
+{
 
-	static String VelsoURL ="https://velso.thyrocare.cloud";
+	static String BaseURL;
 	static String APIKEY;
+	static String[] TestData = new String[5];
 
+	/*
+	 * @BeforeMethod
+	 * 
+	 * @SuppressWarnings("resource") public String[] GetExcelData(int TestCaseID)
+	 * throws IOException {
+	 * 
+	 * FileInputStream fs = new FileInputStream("C:\\Users\\4482E\\Desktop\\New folder\\TESTDATA.xlsx");
+	 * XSSFWorkbook workbook = new XSSFWorkbook(fs); XSSFSheet sheet =
+	 * workbook.getSheet("API");
+	 * 
+	 * TestData[0]=sheet.getRow(TestCaseID).getCell(2).toString();
+	 * TestData[1]=sheet.getRow(TestCaseID).getCell(3).toString();
+	 * TestData[2]=sheet.getRow(TestCaseID).getCell(4).toString();
+	 * 
+	 * System.out.println(TestData);
+	 * 
+	 * return TestData;
+	 * 
+	 * }
+	 */
 
-	@SuppressWarnings("unchecked")
-	@Test
-	void GetAccessToken()
+	@SuppressWarnings("resource")
+	@Test void POSTAPIVerification() throws IOException, ParseException 
 	{
-		//Specify The Base URL
-		RestAssured.baseURI=VelsoURL+"/api/Login/Login";
-		RequestSpecification Getrequest = RestAssured.given();		
-		JSONObject RequestBody = new JSONObject();	
+			
 
-		RequestBody.put("UserName","9004844180");
-		RequestBody.put("Password","123456");
-		RequestBody.put("userType","DSA");
-		RequestBody.put("portalType","DSA");
+		FileInputStream fs = new FileInputStream("C:\\Users\\4482E\\Desktop\\New folder\\TESTDATA.xlsx");
+		XSSFWorkbook workbook = new XSSFWorkbook(fs);
+		XSSFSheet sheet = workbook.getSheet("API");
 
+		TestData[0]=sheet.getRow(1).getCell(2).toString();
+		TestData[1]=sheet.getRow(1).getCell(3).toString();
+		TestData[2]=sheet.getRow(1).getCell(4).toString();
+		System.out.println(TestData[2]);
+
+		RestAssured.baseURI=TestData[0];
+		System.out.println(TestData[0]);
+		
+		RequestSpecification Getrequest = RestAssured.given(); 
+		
+		JSONParser testobj = new JSONParser();
+		
+		JSONObject RequestBody = (JSONObject) testobj.parse(TestData[2]);
+		
 		Getrequest.header("Content-Type","application/json");
-
-		Getrequest.body(RequestBody.toJSONString());
+		
+		Getrequest.body(RequestBody.toString());
+		
 
 		Response response = Getrequest.request(Method.POST);
+		
+		ResponseBody resobj =  response.getBody();
 
-		APIKEY= response.jsonPath().get("apiKey");
+		//APIKEY= response.jsonPath().get("apiKey");
 
-		System.out.println(APIKEY);
+		System.out.println(resobj.asString());
 
 	}
-
 
 
 }
